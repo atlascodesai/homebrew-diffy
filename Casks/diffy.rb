c3a37659e2a37e1cd15e262642ccd50e6f9dd8bd
atlascodesai <atlascodesai@users.noreply.github.com>
2025-11-09 14:52:09 +0000

    Fix arch variable for correct URL

--- Casks/diffy.rb
+++ Casks/diffy.rb
@@ -3,7 +3,9 @@ cask "diffy" do
   sha256 arm:   "51d44eadad17052ff65f1674136b52163a93d117b94ae8fd8aab2d7beb18b8b3",
          intel: "c6fb6b93ccb95a6aab98d03bb726e3d720ba42b80d67363fc4a2d62bec149594"
 
-  url "https://github.com/atlascodesai/diffy/releases/download/v#{version}/diffy-#{Hardware::CPU.arch}-apple-darwin.tar.gz"
+  arch arm: "aarch64", intel: "x86_64"
+
+  url "https://github.com/atlascodesai/diffy/releases/download/v#{version}/diffy-#{arch}-apple-darwin.tar.gz"
   name "Diffy"
   desc "Modern, native diff tool for macOS"
   homepage "https://github.com/atlascodesai/diffy"
